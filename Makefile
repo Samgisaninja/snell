5c78c8beb8f2fb9eb6e643327e93f620dfb8dc28
Sam Gardner <stgardner4@att.net>
2020-01-04 00:06:39 -0600

    init prefs

--- Makefile
+++ Makefile
@@ -10,3 +10,5 @@ snell_PRIVATE_FRAMEWORKS = AppSupport
 export TARGET = iphone:12.2:10.0
 
 include $(THEOS_MAKE_PATH)/tweak.mk
+SUBPROJECTS += snellprefs
+include $(THEOS_MAKE_PATH)/aggregate.mk
---
+++ snellprefs/SNLRootListController.m
@@ -0,0 +1,13 @@
+#include "SNLRootListController.h"
+
+@implementation SNLRootListController
+
+- (NSArray *)specifiers {
+	if (!_specifiers) {
+		_specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+	}
+
+	return _specifiers;
+}
+
+@end
